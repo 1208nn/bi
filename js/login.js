46632729398a6a298ea4196bc26c8cb59bf81fbd
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-01 11:45:09 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function login() {
     var pwd = document.getElementById("pwd").value;
     var ip = document.getElementById("userAgentIp").innerHTML;
-    if (pwd == "{{ site.pwd }}" && { % include iplist.html %}) {
+    if ({ % include iplist.html %}) {
         location.href = "http://magazine.brothers.gq";
     } else {
         alert('Wrong Password/IP');
