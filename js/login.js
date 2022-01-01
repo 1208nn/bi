03c0f87ba38b81f6c378d648a0a5816ee899e195
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-01 11:55:13 +0800

    Delete login.js

--- js/login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    var pwd = document.getElementById("pwd").value;
-    var ip = document.getElementById("userAgentIp").innerHTML;
-    if (pwd == "{{ site.pwd }}" && { % include iplist.html %}) {
-        location.href = "http://magazine.brothers.gq";
-    } else {
-        alert('Wrong Password/IP');
-    }
-}
