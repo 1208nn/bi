e5723176dc77ee5caa17f52de62032883facd015
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-03 14:08:21 +0800

    Update logverify.js

--- js/logverify.js
+++ js/logverify.js
@@ -1,9 +1,3 @@
-function logverify(logintime , utoken) {
-    var token = {{ site.token }};
-    //var ip = 
-    var time = getTime() - logintime;
-    if ((time > 3600000)||!(token == utoken)){
-        alert ("Please login again. ");
-        
-    }
+function logverify() {
+    ut= getCookie("token");
 }
