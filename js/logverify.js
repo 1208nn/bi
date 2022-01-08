e506ead62f350bdadc96eb8fec31fcdfbd42c31e
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-08 20:35:17 +0800

    Update logverify.js

--- js/logverify.js
+++ js/logverify.js
@@ -3,9 +3,9 @@ function logverify() {
     var utoken = getCookie("token");
     var ip = document.getElementById("userAgentIp").innerHTML;
     if (!(utoken == "{{ site.pwd }}{{ site.token }}" && {% include iplist.dat %})){
-       document.getElementById("ulogstate").innerHTML="<a href=\"https://insiders.brothers.gq/login/\">Login</a>";
+        document.getElementById("ulogstate").innerHTML="<a href=\"https://insiders.brothers.gq/login/\">Login</a>";
     } else {
-        document.getElementById("ulogstate").innerHTML="Logged";
+        document.getElementById("ulogstate").innerHTML="Logged <a href=\"javascript:setCookie(\"token\",\"\",5);\">Logout</a><meta http-equiv=\"refresh\" content=\"0;url= / \">";
     }
 }
 //location.href = ("https://insiders.brothers.gq/login/");
