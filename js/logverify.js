ee81a64e45eb08dc6c16c1aac1f99493696ca223
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-08 19:32:35 +0800

    Update logverify.js

--- js/logverify.js
+++ js/logverify.js
@@ -1,4 +1,8 @@
 function logverify() {
+    getIpInfo();
     var utoken = getCookie("token");
-    ip =;
+    var ip = document.getElementById("userAgentIp").innerHTML;
+    if (!(utoken == "{{ site.pwd }}{{ site.token }}" && {% include iplist.dat %})){
+        location.href = ("https://insiders.brothers.gq/login/");
+    }
 }
