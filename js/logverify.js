eec0d20e29090bd3165a0c1bdf05a788ca32b98c
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-08 20:52:38 +0800

    Update logverify.js

--- js/logverify.js
+++ js/logverify.js
@@ -1,5 +1,4 @@
 function logverify() {
-    getIpInfo();
     var utoken = getCookie("token");
     var ip = document.getElementById("userAgentIp").innerHTML;
     if (!(utoken == "{{ site.pwd }}{{ site.token }}" && {% include iplist.dat %})){
