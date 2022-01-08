eebeddaac54518db3d50c366a741ccba2f8a7db0
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-08 21:14:36 +0800

    Update logverify.js

--- js/logverify.js
+++ js/logverify.js
@@ -1,7 +1,8 @@
 function logverify() {
     var utoken = getCookie("token");
     var ip = document.getElementById("userAgentIp").innerHTML;
-    if (!(utoken == "{{ site.pwd }}{{ site.token }}" && {% include iplist.dat %})){
+    if (utoken != "{{ site.pwd }}{{ site.token }}" || ! {% include iplist.dat %}){
+        setCookie("token","",5);
         document.getElementById("ulogstate").innerHTML="<a href=\"https://insiders.brothers.gq/login/\">Login</a>";
     } else {
         document.getElementById("ulogstate").innerHTML="Logged <a href=\"javascript:setCookie(\"token\",\"\",5);\">Logout</a><meta http-equiv=\"refresh\" content=\"0;url= / \">";
