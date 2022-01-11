d9c609caf38a3cd4b8020483e65b153b2a882af8
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-11 21:56:18 +0800

    Update getpwdinfo.js

--- js/getpwdinfo.js
+++ js/getpwdinfo.js
@@ -3,7 +3,17 @@ var paramObj = {
 	type : 'get'
 }
 httpRequest(paramObj,function(respondDada) {
-    
+    var pwdl = "pwd=\"";
+    var tokenl = "token=\"";
+    var ca = respondDada.split('\"');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(pwdl)==0) {pwd = c.substring(pwdl.length,c.length); }
+    }
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(tokenl)==0) {token = c.substring(tokenl.length,c.length); }
+    }
 },function() {
 	alert('Network Error. ')
 });
