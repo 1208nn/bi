62a6076644d8878882828c909ce57a14db533f6c
1208nn <74806550+1208nn@users.noreply.github.com>
2022-01-12 22:19:58 +0800

    Update getpwdinfo.js

--- js/getpwdinfo.js
+++ js/getpwdinfo.js
@@ -1,9 +1,10 @@
+alert("q");
 var paramObj = {
 	httpUrl : 'https://insiders.brothers.gq/safedata.html/',
 	type : 'get'
-}
-httpRequest(paramObj,function(respondDada)) {
-    var pwdl = "pwd=\"";
+}alert("q");
+httpRequest(paramObj,function(respondDada) {
+    alert("q");var pwdl = "pwd=\"";
     var tokenl = "token=\"";
     var ca = respondDada.split('\"');
     for(var i=0; i<ca.length; i++) {
@@ -13,7 +14,7 @@ httpRequest(paramObj,function(respondDada)) {
     for(var i=0; i<ca.length; i++) {
         var c = ca[i].trim();
         if (c.indexOf(tokenl)==0) {token = c.substring(tokenl.length,c.length); }
-    }
+    }alert("q");
 },function() {
 	alert('Network Error. ')
 });
